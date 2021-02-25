e01dc926383e6992241b8c5d7c1118fe902e3e7f
LNow <>
2021-02-25 23:30:32 +0100

    Added a lock for loading the script multiple times

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export PS4='\nDEBUG level:$SHLVL subshell-level: $BASH_SUBSHELL \nsource-file:${BASH_SOURCE} line#:${LINENO} function:${FUNCNAME[0]:+${FUNCNAME[0]}(): }\nstatement: '
 
+# return if script is already soruced
+[ -n "$COMPAGE_LOADED" ] && return || COMPAGE_LOADED=1
+
 readonly COMPAGE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly COMPAGE_CORE_PATH="${COMPAGE_PATH}/Core"
 readonly COMPAGE_LIB_PATH="${COMPAGE_PATH}/Lib"
